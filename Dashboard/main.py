9090f0c56ff839f31e70879b23dc4c9701f4bd48
KARTIKEYA JAIN <93050233+KARTIKEYA47JAIN@users.noreply.github.com>
2022-12-15 22:34:30 +0530

    Update main.py

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -18,9 +18,6 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import warnings
-import ssl
-
-ssl.match_hostname = lambda cert, hostname: True
 
 warnings.filterwarnings('ignore')
 
